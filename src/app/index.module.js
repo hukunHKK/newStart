06cfd02e9a3cdcfc47ddfeb418fc911940e1f127
211998263@qq.com <294418442@qq.com>
2017-07-28 12:26:16 +0800

    新搭建框架

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('newStart', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
